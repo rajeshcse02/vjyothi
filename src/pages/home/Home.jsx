c2cf5d56a2577f301d9e6b309dc16cfd9bbc876b
rajeshrcse <r21161500@gmail.com>
2025-01-06 20:53:57 +0530

    for SEO Changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,47 +6,57 @@ import ProductCard from "../../components/productCard/ProductCard";
 import { Link } from "react-router-dom";
 import "../home/home.css";
 import Category from "../../components/category/Category";
+import { Helmet } from "react-helmet";
 
 // Functional component for the home page
 function Home() {
   const context = useContext(myContext);
   const { loading, setLoading } = context;
   return (
-    <Layout>
-      {/* Hero section at the top of the page with fade-down animation */}
-      <div className="fade-down">
-        <HeroSection />
-      </div>
-      {/* ProductCard component displaying product cards with fade-down animation */}
-      {loading ? (
-        <div className="spinner">
-          <div className="inner-spin"></div>
-        </div>
-      ) : (
+    <>
+      <Helmet>
+        <title>V Jyothi Sarees - Only Silk Cotton</title>
+        <meta
+          name="description"
+          content="Welcome to V Jyothi Sarees - The best place for silk cotton sarees!"
+        />
+      </Helmet>
+      <Layout>
+        {/* Hero section at the top of the page with fade-down animation */}
         <div className="fade-down">
-          <ProductCard />
+          <HeroSection />
         </div>
-      )}
+        {/* ProductCard component displaying product cards with fade-down animation */}
+        {loading ? (
+          <div className="spinner">
+            <div className="inner-spin"></div>
+          </div>
+        ) : (
+          <div className="fade-down">
+            <ProductCard />
+          </div>
+        )}
 
-      {/* Button to navigate to the "allproducts" page with fade-down animation */}
-      <div className="flex justify-center -mt-10 mb-4 fade-down">
-        <Link to={"/allproducts"}>
-          <button className="text-white text-xl hover:bg-violet-500 transition ease-in-out bg-pink-500 px-10 py-3 m-9 rounded-3xl">
-            See more
-          </button>
-        </Link>
-      </div>
+        {/* Button to navigate to the "allproducts" page with fade-down animation */}
+        <div className="flex justify-center -mt-10 mb-4 fade-down">
+          <Link to={"/allproducts"}>
+            <button className="text-white text-xl hover:bg-violet-500 transition ease-in-out bg-pink-500 px-10 py-3 m-9 rounded-3xl">
+              See more
+            </button>
+          </Link>
+        </div>
 
-      {/* Track component for tracking information with fade-down animation */}
-      <div className="fade-down">
-        <Category />
-      </div>
+        {/* Track component for tracking information with fade-down animation */}
+        <div className="fade-down">
+          <Category />
+        </div>
 
-      {/* Testimonial component (commented out for now) with fade-down animation */}
-      {/* <div className="fade-down">
+        {/* Testimonial component (commented out for now) with fade-down animation */}
+        {/* <div className="fade-down">
         <Testimonial />
       </div> */}
-    </Layout>
+      </Layout>
+    </>
   );
 }
 
